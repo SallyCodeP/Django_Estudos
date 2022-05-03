4b13a910d0ebc55827388977290b2436bad296fb
Paulo Gentil <paulogentil@MacBook-Pro-de-Paulo.local>
2022-05-03 16:51:13 -0300

    request no react para a api django

--- rooms/ui/src/App.tsx
+++ rooms/ui/src/App.tsx
@@ -3,14 +3,31 @@ import logo from './logo.svg';
 import './App.css';
 import styled from 'styled-components';
 import InputSeiLa from './Components/InputSeiLa';
+import axios from 'axios';
+
+// Ensinando request :)
+
+
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 type AppProps = {
   className?: string;
 
 }
+
   
 
 const App = ({className}:AppProps) => {
+
+
+  const getRequest = () => {
+    axios.get(`127.0.0.1:8000/room/login/user`)
+    .then(response => console.log(response.data))
+    .catch(err => console.log("DEU RUIM"))
+  }
+
+
+  
   return (
     <div 
     className={className}>
@@ -20,7 +37,7 @@ const App = ({className}:AppProps) => {
     placeHolder='eu sou um pato?'
     type='text'
     />
-    
+    <button onClick={() => getRequest()}> ENVIARA </button>
     </div>
   );
 }
